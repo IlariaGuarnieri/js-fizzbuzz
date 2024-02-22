b58e066252fb48f1562172bade43cbbe36b709ff
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-22 17:29:13 +0100

    [add]

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
+
 const containerQ = document.getElementById('container-q');
 for(let i = 1; i <= 100; i++){
   let classDispari= '';
   if(i % 3 === 0){
-    classDispari += 'fizz';
-  }
-  if(i % 5 0=== 0){
-    classDispari += 'buzz'
+    classDispari= 'fizz';
   }
 
   containerQ.innerHTML += `
-    <div class="box">${classDispari||i}</div>
+    <div class="box">${classDispari}${i}</div>
   `
 }
 
