2e69688a8491f07798fd5287378ff77ff5416068
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-02-23 09:39:03 +0100

    [add] finite le modifiche

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 
 const containerQ = document.getElementById('container-q');
 for(let i = 1; i <= 100; i++){
-  let classDispari= '';
-  if(i % 3 === 0){
-    classDispari= 'fizz';
+  let stampa
+  if(i % 3 === 0 && i % 5 ===0){
+    stampa= 'fizzBuzz';
+  }
+  else if(i % 5 === 0){
+    stampa= 'buzz';
+  }
+  else if( i % 3 === 0){
+    stampa = 'fizz'
+  }
+  else {
+    stampa = i
   }
 
+
   containerQ.innerHTML += `
-    <div class="box">${classDispari}${i}</div>
+    <div class="box ${stampa}">${stampa}</div>
   `
 }
 
+// const containerQ = document.getElementById('container-q');
+// for(let i = 1; i <= 100; i++){
+//   let classFive= '';
+//   if(i % 5 === 0){
+//     classFive= 'buzz';
+//   }
+
+//   containerQ.innerHTML += `
+//     <div class="box">${classFive}${i}</div>
+//   `
+// }
